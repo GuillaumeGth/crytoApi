6e165040bff568193f23c68daa3349a2e6e8bba4
Guillaume D. Gth <guillaume.zarb@gmail.com>
2022-01-01 21:21:10 +0100

    cleaning

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 namespace api.Controllers {
-    public class BaseApiController : ControllerBase {
+    public class BaseApiController : ControllerBase {        
         protected DatabaseContext Context;
         protected IHttpContextAccessor ContextAccessor;
         protected readonly ILogger<CryptoController> Logger;
 
-        public BaseApiController(ILogger<CryptoController> logger,
+        protected BaseApiController(ILogger<CryptoController> logger,
         DatabaseContext context, 
         IHttpContextAccessor contextAccessor)
         {
--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
-
+using System;
 namespace api
 {
     public class Startup
@@ -29,16 +29,32 @@ namespace api
                 .AllowAnyMethod()
                 .AllowAnyHeader();
             }));
-            services.AddControllers(); 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            string herokuConnectionString = $@"
-                Server={Configuration["PostgreSql:Host"]};
-                Port={Configuration["PostgreSql:Port"]};
-                User Id={Configuration["PostgreSql:User"]};
-                Password={Configuration["PostgreSql:ServerPassword"]};
-                Database={Configuration["PostgreSql:DatabaseName"]};
-                SSL Mode=Require;Trust Server Certificate=true";        
-            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder(herokuConnectionString);           
+            services.AddControllers();             
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();        
+            string databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL") ?? Configuration["PostgreSql:DATABASE_URL"];            
+            Uri databaseUri = null;
+            string[] userInfo = {null, null};
+            if (databaseUrl != null){
+                databaseUri = new Uri(databaseUrl);                
+                if (databaseUri != null)
+                    userInfo = databaseUri.UserInfo.Split(':');
+            }           
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri?.Host ?? Configuration["PostgreSql:Host"],
+                Port = int.Parse(databaseUri?.Port.ToString() ?? Configuration["PostgreSql:Port"]),
+                Username = userInfo[0] ?? Configuration["PostgreSql:User"],
+                Password = userInfo[1] ?? Configuration["PostgreSql:ServerPassword"],
+                Database = databaseUri.LocalPath.TrimStart('/') ?? Configuration["PostgreSql:DatabaseName"]
+            };
+            // string herokuConnectionString = $@"
+            //     Server={Configuration["PostgreSql:Host"]};
+            //     Port={Configuration["PostgreSql:Port"]};
+            //     User Id={Configuration["PostgreSql:User"]};
+            //     Password={Configuration["PostgreSql:ServerPassword"]};
+            //     Database={Configuration["PostgreSql:DatabaseName"]};
+            //     SSL Mode=Require;Trust Server Certificate=true";        
+            // NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder(herokuConnectionString);                       
             services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(builder.ConnectionString));        
             services.AddSwaggerGen(c =>
             {
@@ -54,8 +70,7 @@ namespace api
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
-            }
-            app.UseCors("MyPolicy");
+            }                      
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
